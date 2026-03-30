dd89ad6a8ebb25871b314c77221c4e59ba4cde3b
František Bereň <birko@live.com>
2026-03-30 08:49:11 +0200

    refactor: remove GenerateSlug and RemoveDiacritics, moved to SlugGenerator in Birko.Data.Patterns

--- StringHelper.cs
+++ StringHelper.cs
@@ -1,68 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace Birko.Helpers
 {
     public static class StringHelper
     {
-        static readonly Regex WordDelimiters = new Regex(@"[\s—–_\/]", RegexOptions.Compiled);
-
-        // characters that are not valid
-        static readonly Regex InvalidChars = new Regex(@"[^a-z0-9\-]", RegexOptions.Compiled);
-
-        // multiple hyphens
-        static readonly Regex MultipleHyphens = new Regex(@"-{2,}", RegexOptions.Compiled);
-
-        public static string GenerateSlug(this string value)
-        {
-            if (string.IsNullOrEmpty(value))
-            {
-                return string.Empty;
-            }
-            // convert to lower case
-            value = value.ToLowerInvariant();
-
-            // remove diacritics (accents)
-            value = RemoveDiacritics(value);
-
-            // ensure all word delimiters are hyphens
-            value = WordDelimiters.Replace(value, "-");
-
-            // strip out invalid characters
-            value = InvalidChars.Replace(value, "");
-
-            // replace multiple hyphens (-) with a single hyphen
-            value = MultipleHyphens.Replace(value, "-");
-
-            // trim hyphens (-) from ends
-            return value.Trim('-');
-        }
-
-        public static string RemoveDiacritics(this string text)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                return string.Empty;
-            }
-            var normalizedString = text.Normalize(NormalizationForm.FormD);
-            var stringBuilder = new StringBuilder();
-
-            foreach (var c in normalizedString)
-            {
-                var unicodeCategory = CharUnicodeInfo.GetUnicodeCategory(c);
-                if (unicodeCategory != UnicodeCategory.NonSpacingMark)
-                {
-                    stringBuilder.Append(c);
-                }
-            }
-            return stringBuilder.ToString().Normalize(NormalizationForm.FormC);
-        }
-
         public static string RemoveMultipleSpaces(this string str)
         {
             if (string.IsNullOrEmpty(str))
